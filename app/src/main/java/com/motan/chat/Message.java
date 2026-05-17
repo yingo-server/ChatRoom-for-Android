a0a388803482ef074f767f761fb84d5fea6d43db
yingo-server <18674211622@163.com>
2026-05-17 20:39:52 +0800

    Add Message class for chat functionality

---
+++ app/src/main/java/com/motan/chat/Message.java
@@ -0,0 +1,14 @@
+package com.motan.chat;
+
+import java.util.List;
+
+public class Message {
+    public String id;
+    public String userId;
+    public String username;
+    public String text;
+    public List<String> images;
+    public String audio;
+    public long timestamp;
+    public String time;
+}
